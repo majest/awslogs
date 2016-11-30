2d288c120207f91d11f65e441d0c81696426ce64
Artur <majest.pl@gmail.com>
2016-11-30 09:38:52 +0000

    env names fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -t --rm -e AWS_ACCESS_KEY=key -e AWS_SECRET_KEY=secret -e AWS_REGION=region awslogs awslogs get YOUR_LOG_GROUP --watch
+docker run -t --rm -e AWS_ACCESS_KEY_ID=key -e AWS_SECRET_ACCESS_KEY=secret -e AWS_REGION=region awslogs awslogs get YOUR_LOG_GROUP --watch
